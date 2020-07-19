398ae797208264a08d0c4b008b083cfbbc6a402b
eneax <19818937+eneax@users.noreply.github.com>
2020-07-19 16:59:21 +0200

    Update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Dev Portfolio Starter`,
+    title: "Dev Portfolio",
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@eneax`,
+    twitterUsername: "@eneaxharja",
+    image: "/twitter-img.png",
+    siteUrl: "http://localhost:9000",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -18,13 +22,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Dev Portfolio`,
+        short_name: `Dev`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `src/assets/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,

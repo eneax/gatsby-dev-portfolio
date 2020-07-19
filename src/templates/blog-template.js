fb2fae1aa8a8e5d4048501525cf101968210910b
eneax <19818937+eneax@users.noreply.github.com>
2020-07-19 17:20:33 +0200

    Refactor template

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,19 +1,11 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+
 import Layout from "../components/layout"
-import ReactMarkdown from "react-markdown"
 
 const ComponentName = () => {
   return <h2>blog post template</h2>
 }
 
-// export const query = graphql`
-//   query GetSingleBlog($slug: String) {
-//     blog: strapiBlogs(slug: { eq: $slug }) {
-//       content
-
-//     }
-//   }
-// `
 
 export default ComponentName

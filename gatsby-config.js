4241589db8cf1470f3a22826bab19c914cbbe814
eneax <19818937+eneax@users.noreply.github.com>
2020-08-23 16:55:10 +0200

    Config gatsby-source-strapi plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`jobs`],
+      },
+    },
   ],
-}
+};

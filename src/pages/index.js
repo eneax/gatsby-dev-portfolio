ae2a7829928286b35fff40e2eec7eee4ecae14a1
eneax <19818937+eneax@users.noreply.github.com>
2020-09-03 16:12:15 +0200

    Set projects query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Hero from '../components/hero';
 import Services from '../components/services';
 import Jobs from '../components/jobs';
+import Projects from '../components/projects';
 
-const HomePage = () => (
-  <Layout>
-    <SEO title="Home" />
+export const getProjectsData = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        id
+        title
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        github
+        url
+        stack {
+          id
+          item
+        }
+      }
+    }
+  }
+`;
 
-    <Hero />
-    <Services />
-    <Jobs />
-  </Layout>
-);
+const HomePage = ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data;
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+
+      <Hero />
+      <Services />
+      <Jobs />
+      <Projects title="featured projects" projects={projects} showLink />
+    </Layout>
+  );
+};
+
+HomePage.propTypes = {
+  data: PropTypes.shape({
+    allStrapiProjects: PropTypes.object.isRequired,
+  }).isRequired,
+};
 
 export default HomePage;

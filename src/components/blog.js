3e0ebf3b9fccfaaaa599203d1dfe6787e1fa3060
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:00:15 +0200

    Use styled-components in blog component

--- src/components/blog.js
+++ src/components/blog.js
@@ -2,9 +2,82 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Image from 'gatsby-image';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+// styled
+const BlogSection = styled(Link)`
+  display: block;
+  margin-bottom: 2rem;
+  background: var(--clr-grey-10);
+  border-radius: var(--radius);
+  box-shadow: var(--light-shadow);
+  transition: var(--transition);
+
+  &:hover {
+    box-shadow: var(--dark-shadow);
+    transform: scale(1.02);
+  }
+
+  article {
+    height: 100%;
+    display: grid;
+    grid-template-rows: auto 1fr;
+  }
+
+  .blog-img {
+    border-top-left-radius: var(--radius);
+    border-top-right-radius: var(--radius);
+    height: 15rem;
+  }
+
+  .blog-card {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    padding: 1.5rem 1rem;
+
+    h4 {
+      color: var(--clr-grey-1);
+    }
+  }
+
+  .blog-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    p {
+      margin-bottom: 0;
+      font-weight: bold;
+      color: var(--clr-grey-5);
+
+      &:first-of-type {
+        display: inline-block;
+        background: var(--clr-grey-9);
+        color: var(--clr-grey-5);
+        margin-right: 0.5rem;
+        padding: 0.25rem 0.5rem;
+        border-radius: var(--radius);
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        font-size: 0.85rem;
+      }
+    }
+  }
+
+  @media screen and (min-width: 576px) {
+    .blog-img {
+      height: 17.5rem;
+    }
+  }
+  @media screen and (min-width: 850px) {
+    .blog-img {
+      height: 13.75rem;
+    }
+  }
+`;
 
 const Blog = ({ id, title, image, date, category, slug, desc }) => (
-  <Link to={`/blogs/${slug}`} className="blog" key={id}>
+  <BlogSection to={`/blogs/${slug}`} className="blog" key={id}>
     <article>
       {image && (
         <Image fluid={image.childImageSharp.fluid} className="blog-img" />
@@ -18,7 +91,7 @@ const Blog = ({ id, title, image, date, category, slug, desc }) => (
         </div>
       </div>
     </article>
-  </Link>
+  </BlogSection>
 );
 
 Blog.propTypes = {

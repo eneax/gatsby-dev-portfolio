71b1aed148736d03d88658dd13fd8a7e55892d2d
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:01:19 +0200

    Refactor hero to use styled-components

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,89 @@
 import React from 'react';
 import { useStaticQuery, graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 
 import SocialLinks from './socialLinks';
 
+// styles
+const HeaderHero = styled.header`
+  margin-top: -5rem;
+  padding-top: 5rem;
+  height: 100vh;
+  background: var(--clr-primary-10);
+  position: relative;
+
+  .hero-center {
+    height: 100%;
+    display: grid;
+    align-items: center;
+  }
+
+  .hero-info {
+    background: var(--clr-primary-10);
+
+    h4 {
+      color: var(--clr-grey-5);
+    }
+  }
+
+  .underline {
+    margin-bottom: 0.5rem;
+    margin-left: 0;
+  }
+
+  .btn {
+    margin-top: 1.25rem;
+  }
+
+  .hero-img {
+    display: none;
+  }
+
+  @media screen and (min-width: 992px) {
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 65%;
+      right: 0;
+      bottom: 0;
+      background: var(--clr-white);
+    }
+
+    .hero-center {
+      grid-template-columns: repeat(12, 1fr);
+    }
+
+    .hero-info {
+      grid-row: 1/1;
+      grid-column: 1 / span 8;
+
+      h4 {
+        font-size: 0.85rem;
+      }
+    }
+
+    .hero-img {
+      display: block;
+      grid-row: 1/1;
+      grid-column: 6/-1;
+    }
+  }
+
+  @media screen and (min-width: 1170px) {
+    .hero-info {
+      grid-column: 1 / span 8;
+
+      h4 {
+        font-size: 1rem;
+        line-height: 1;
+      }
+    }
+  }
+`;
+
+// query
 const getHeroImg = graphql`
   {
     heroImg: file(relativePath: { eq: "hero-img.png" }) {
@@ -21,14 +101,14 @@ const Hero = () => {
   const heroImg = response.heroImg.childImageSharp.fluid;
 
   return (
-    <header className="hero">
+    <HeaderHero>
       <div className="section-center hero-center">
         <article className="hero-info">
           <div>
             <div className="underline" />
             <h1>Hi, I'm John</h1>
             <h4>Web Developer</h4>
-            <Link to="contact" className=" btn">
+            <Link to="contact" className="btn">
               Contact Me
             </Link>
             <SocialLinks />
@@ -36,7 +116,7 @@ const Hero = () => {
         </article>
         <Img fluid={heroImg} className="hero-img" />
       </div>
-    </header>
+    </HeaderHero>
   );
 };
 

c53b6b5c19bd26dd85f3a079be0c0ce47cae43d4
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:02:45 +0200

    Refactor about page to use styled-components

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,91 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Title from '../components/title';
 
+// styles
+const AboutSection = styled.section`
+  padding: 7rem 0;
+
+  .about-img {
+    margin-bottom: 2rem;
+    height: 25rem;
+  }
+
+  .about-text {
+    .section-title {
+      margin-bottom: 2rem;
+      text-align: left;
+    }
+
+    .underline {
+      margin-left: 0;
+      margin-right: 0;
+    }
+
+    p {
+      line-height: 2;
+    }
+  }
+
+  .stack-container {
+    width: 30rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    row-gap: 0.5rem;
+
+    span {
+      text-transform: uppercase;
+      display: flex;
+      align-items: center;
+      letter-spacing: 1px;
+      color: var(--clr-grey-4);
+    }
+  }
+
+  .stack-icon {
+    color: var(--clr-primary-5);
+    margin-right: 0.25rem;
+    font-size: 1rem;
+  }
+
+  .about-stack {
+    span {
+      display: inline-block;
+      background: var(--clr-grey-9);
+      color: var(--clr-grey-5);
+      margin-top: 0.5rem;
+      margin-right: 0.5rem;
+      padding: 0.25rem 0.5rem;
+      border-radius: var(--radius);
+      text-transform: uppercase;
+      letter-spacing: 2px;
+      font-size: 0.85rem;
+    }
+  }
+
+  @media screen and (min-width: 992px) {
+    .about-center {
+      display: grid;
+      grid-template-columns: repeat(12, 1fr);
+      align-items: center;
+      column-gap: 4rem;
+    }
+    .about-img {
+      grid-column: 1 / span 5;
+      margin-bottom: 0;
+    }
+    .about-text {
+      grid-column: 6 / -1;
+    }
+  }
+`;
+
+// query
 export const getAboutData = graphql`
   {
     about: allStrapiAbout {
@@ -38,7 +118,8 @@ const About = ({ data }) => {
   return (
     <Layout>
       <SEO title="About Me" description="about webdev" />
-      <section className="about-page">
+
+      <AboutSection className="about-page">
         <div className="section-center about-center">
           <Img fluid={image.childImageSharp.fluid} className="about-img" />
           <article className="about-text">
@@ -51,7 +132,7 @@ const About = ({ data }) => {
             </div>
           </article>
         </div>
-      </section>
+      </AboutSection>
     </Layout>
   );
 };

1a656c911ce184811e9403861cca115dbba0ff7e
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:03:08 +0200

    Refactor contact page to use styled-components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,73 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+// styles
+const ContactSection = styled.section`
+  display: grid;
+  place-items: center;
+  padding: 5rem 0;
+
+  .contact-form {
+    background: var(--clr-white);
+    border-radius: var(--radius);
+    text-align: center;
+    box-shadow: var(--light-shadow);
+    transition: var(--transition);
+    width: 90vw;
+    max-width: 35rem;
+
+    &:hover {
+      box-shadow: var(--dark-shadow);
+    }
+
+    h3 {
+      padding-top: 1.25rem;
+      color: var(--clr-grey-5);
+    }
+  }
+
+  .form-group {
+    padding: 1rem 1.5rem;
+  }
+
+  .form-control {
+    display: block;
+    width: 100%;
+    padding: 0.75rem 1rem;
+    border: none;
+    margin-bottom: 1.25rem;
+    background: var(--clr-grey-10);
+    border-radius: var(--radius);
+    text-transform: uppercase;
+    letter-spacing: var(--spacing);
+
+    &:placeholder {
+      font-family: var(--ff-primary);
+      color: var(--clr-grey-1);
+      text-transform: uppercase;
+      letter-spacing: var(--spacing);
+    }
+  }
+
+  .submit-btn {
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border-bottom-left-radius: var(--radius);
+    border-bottom-right-radius: var(--radius);
+    border-top-right-radius: 0;
+    border-top-left-radius: 0;
+  }
+`;
+
 const Contact = () => (
   <Layout>
     <SEO title="Contact" />
-    <section className="contact-page">
+
+    <ContactSection className="contact-page">
       <article className="contact-form">
         <h3>get in touch</h3>
         <form name="contact" method="POST" data-netlify="true">
@@ -30,12 +91,12 @@ const Contact = () => (
               className="form-control"
             />
           </div>
-          <button type="submit" className="submit-btn btn">
+          <button type="submit" className="btn submit-btn">
             submit here
           </button>
         </form>
       </article>
-    </section>
+    </ContactSection>
   </Layout>
 );
 

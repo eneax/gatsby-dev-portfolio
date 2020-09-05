399c9da4178be29e0bceb85c3885272b6a60dcac
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:02:24 +0200

    Refactor services to use styled-components

--- src/components/services.js
+++ src/components/services.js
@@ -1,8 +1,67 @@
 import React from 'react';
+import styled from 'styled-components';
 import { FaCode, FaSketch, FaAndroid } from 'react-icons/fa';
 
 import Title from './title';
 
+const ServiceWrapper = styled.div`
+  @media screen and (min-width: 676px) {
+    .services-center {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      -webkit-column-gap: 2rem;
+      -moz-column-gap: 2rem;
+      column-gap: 2rem;
+    }
+  }
+  @media screen and (min-width: 992px) {
+    .services-center {
+      grid-template-columns: 1fr 1fr 1fr;
+    }
+  }
+`;
+
+const ServiceArticle = styled.article`
+  background: var(--clr-white);
+  padding: 3rem 1.5rem;
+  margin-bottom: 2rem;
+  border-radius: var(--radius);
+  text-align: center;
+  -webkit-transition: var(--transition);
+  transition: var(--transition);
+
+  .service-icon {
+    font-size: 2rem;
+    margin-bottom: 1.5rem;
+  }
+
+  .underline {
+    width: 3rem;
+    height: 0.12rem;
+    -webkit-transition: var(--transition);
+    transition: var(--transition);
+  }
+
+  p {
+    -webkit-transition: var(--transition);
+    transition: var(--transition);
+  }
+
+  &:hover {
+    background: var(--clr-primary-5);
+    color: var(--clr-primary-9);
+
+    p {
+      color: var(--clr-primary-9);
+    }
+
+    .underline {
+      background: var(--clr-primary-9);
+    }
+  }
+`;
+
+// data
 const servicesList = [
   {
     id: 1,
@@ -25,20 +84,20 @@ const servicesList = [
 ];
 
 const Services = () => (
-  <section className="section bg-grey">
+  <ServiceWrapper className="section bg-grey">
     <Title title="services" />
 
     <div className="section-center services-center">
       {servicesList.map(({ id, icon, title, text }) => (
-        <article key={id} className="service">
+        <ServiceArticle key={id}>
           {icon}
           <h4>{title}</h4>
           <div className="underline" />
           <p>{text}</p>
-        </article>
+        </ServiceArticle>
       ))}
     </div>
-  </section>
+  </ServiceWrapper>
 );
 
 export default Services;

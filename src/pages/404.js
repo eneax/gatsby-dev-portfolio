817d399d9ae246d8819bf450bac4de677da026f7
eneax <19818937+eneax@users.noreply.github.com>
2020-09-05 17:02:36 +0200

    Refactor 404 page to use styled-components

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+// styles
+const ErrorPageSection = styled.main`
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+
+  h1 {
+    text-transform: uppercase;
+    color: var(--clr-primary-1);
+    margin-bottom: 2rem;
+  }
+`;
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <main className="error-page">
+
+    <ErrorPageSection className="error-page">
       <div className="error-container">
         <h1>NOT FOUND</h1>
         <Link to="/" className="btn">
           back home
         </Link>
       </div>
-    </main>
+    </ErrorPageSection>
   </Layout>
 );
 

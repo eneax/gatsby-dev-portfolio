7b1b04bdaae8b9845e84ad8dfe225452d6bf2933
eneax <19818937+eneax@users.noreply.github.com>
2020-09-06 11:28:20 +0200

    Refactor Navbar to use styled-components

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,107 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import { FaAlignRight } from 'react-icons/fa';
 
 import logo from '../assets/logo.svg';
 import PageLinks from './pageLinks';
 
+// styles
+const NavWrapper = styled.nav`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  z-index: 200;
+  background: var(--clr-white);
+
+  .nav-center {
+    width: 90vw;
+    max-width: 1170px;
+    margin: 0 auto;
+  }
+
+  .nav-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .nav-links {
+    display: none;
+  }
+
+  @media screen and (min-width: 992px) {
+    background: transparent;
+  }
+
+  @media screen and (min-width: 768px) {
+    .nav-links {
+      display: flex;
+      justify-content: flex-end;
+
+      li {
+        margin-right: 2rem;
+      }
+
+      a {
+        text-transform: capitalize;
+        color: var(--clr-grey-1);
+        font-weight: bold;
+        letter-spacing: var(--spacing);
+        transition: var(--transition);
+        padding: 0.5rem 0;
+
+        &:hover {
+          color: var(--clr-primary-5);
+          box-shadow: 0px 2px var(--clr-primary-5);
+        }
+      }
+    }
+
+    .nav-center {
+      display: grid;
+      grid-template-columns: auto 1fr;
+      align-items: center;
+    }
+  }
+`;
+
+const ToggleBtn = styled.button`
+  font-size: 2rem;
+  background: transparent;
+  border-color: transparent;
+  color: var(--clr-primary-5);
+  cursor: pointer;
+  transition: var(--transition);
+
+  &:hover {
+    color: var(--clr-primary-2);
+  }
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
 const Navbar = ({ toggleSidebar }) => (
-  <nav className="navbar">
+  <NavWrapper>
     <div className="nav-center">
       <div className="nav-header">
         <Link to="/">
           <img src={logo} alt="Logo" />
         </Link>
-        <button type="button" className="toggle-btn" onClick={toggleSidebar}>
+        <ToggleBtn onClick={toggleSidebar}>
           <FaAlignRight />
-        </button>
+        </ToggleBtn>
       </div>
       <PageLinks styleClass="nav-links" />
     </div>
-  </nav>
+  </NavWrapper>
 );
 
 Navbar.propTypes = {

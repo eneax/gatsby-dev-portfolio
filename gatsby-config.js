537958d7652fab48e9f49138b3909672fb36ebe8
eneax <19818937+eneax@users.noreply.github.com>
2020-09-06 16:22:29 +0200

    Update logo, facivon and hero-img

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `src/assets/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
